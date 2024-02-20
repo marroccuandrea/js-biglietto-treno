422fcd51cc388eb20ac16162970e69fbcc55abb8
marroccuandrea <marroccuandrea@yahoo.it>
2024-02-20 15:56:37 +0100

    changed distanza int to float

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //1.
-const distanza = parseInt(prompt('Inserisci la distanza che devi percorrere in km'));
+const distanza = parseFloat(prompt('Inserisci la distanza che devi percorrere in km'));
 console.log(distanza);
 
 //2.
@@ -7,6 +7,6 @@ const eta = prompt('Inserisci la tua età');
 console.log(eta);
 
 //3.
-let prezzo = Number(Math.round(parseFloat((distanza * 0,21).toFixed(2))));
+let prezzo = Math.round(parseFloat((distanza * 0,21).toFixed(2)));
 console.log(prezzo);
 
