aa6e0f632f7b02f1fac1bacf56b78078ddfc291c
marroccuandrea <marroccuandrea@yahoo.it>
2024-02-20 16:12:00 +0100

    [add] price calculator

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ const eta = prompt('Inserisci la tua età');
 console.log(eta);
 
 //3.
-let prezzo = Math.round(parseFloat((distanza * 0,21).toFixed(2)));
+let prezzo = parseFloat((distanza * "0.21").toFixed(2));
 console.log(prezzo);
 
