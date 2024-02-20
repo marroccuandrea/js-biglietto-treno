50bb245bc481e578d0b4b7576a9cd9d95e42799d
marroccuandrea <marroccuandrea@yahoo.it>
2024-02-20 17:17:31 +0100

    [add] formula discount

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ console.log(eta);
 let prezzo = parseFloat((distanza * "0.21").toFixed(2));
 console.log(prezzo);
 
+//4.
+
+
+if (eta < 18) {
+    console.log('Applico sconto 20%');
+    sconto = parseFloat(console.log((prezzo * 20) /100));
+}
+else if (eta > 65){
+    console.log('Applico sconto del 40%');
+    sconto = parseFloat(console.log((prezzo * 40) /100));
+}
+else{
+    console.log('Nessuno sconto applicato');
+    sconto = 0;
+}
+const prezzoScontato = parseFloat(prezzo - sconto);
+console.log(prezzoScontato);
+
+document.getElementById('output').innerHTML = prezzoScontato;
